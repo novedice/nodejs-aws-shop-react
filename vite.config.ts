78aa49bff584da5ca25dc881ec6b6b6325cebc55
Natalia Ushakova <natalia@Natalias-Air.home>
2025-02-14 18:32:25 +0000

    fix: change mui version, change source of the photos

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,17 @@ export default defineConfig({
       "~": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
@@ -19,3 +30,4 @@ export default defineConfig({
     setupFiles: "./src/setupTests.ts",
   },
 });
+
